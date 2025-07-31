72b92fbcffbf85f3bf809b7429ad33e407bdacab
AAA <AAA@AAA.COM>
2025-07-31 15:30:49 +0800

    no fid

--- configs/default_cifar10_configs.py
+++ configs/default_cifar10_configs.py
@@ -8,8 +8,8 @@ def get_default_configs():
   config.training = training = ml_collections.ConfigDict()
   config.training.batch_size = 128
   training.n_iters = 1300001
-  training.snapshot_freq = 50000
-  training.log_freq = 50
+  training.snapshot_freq = 1000
+  training.log_freq = 100
   training.eval_freq = 100
   ## store additional checkpoints for preemption in cloud computing environments
   training.snapshot_freq_for_preemption = 10000
@@ -30,9 +30,9 @@ def get_default_configs():
   config.eval = evaluate = ml_collections.ConfigDict()
   evaluate.begin_ckpt = 9
   evaluate.end_ckpt = 26
-  evaluate.batch_size = 1024
+  evaluate.batch_size = 128
   evaluate.enable_sampling = False
-  evaluate.num_samples = 50000
+  evaluate.num_samples = 1000
   evaluate.enable_loss = True
   evaluate.enable_bpd = False
   evaluate.bpd_dataset = 'test'
